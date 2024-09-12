3d5cfef9d55b45ef0b0f683662a486dc6c8f66a8
123AbhiRaghav <abhiraghavd@gmail.com>
2024-09-12 11:23:52 +0530

    first commit

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+// const jwt = require('jsonwebtoken');
+
+// const authenticate = (req, res, next) => {
+//   const token = req.header('Authorization')?.split(' ')[1]; 
+//   if (!token) {
+//     return res.status(401).json({ message: 'No token provided' });
+//   }
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+//     req.user = decoded; 
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: 'Invalid token' });
+//   }
+// };
+
+// module.exports = authenticate;
+
+
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];  
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token missing' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.user = user; 
+    next();
+  });
+};
+
+module.exports = authenticate;
